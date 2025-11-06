d8e0f7378ac42c65bb2ef8ab0db706766b55bb5d
berkayerdemsoy <114877481+berkayerdemsoy@users.noreply.github.com>
2025-11-06 16:32:57 +0300

    WIP: keycloak implementation enhanced for fix dns error

--- backend/order-service/order-service-app/src/main/java/com/example/order_service_app/config/PaymentEventListener.java
+++ backend/order-service/order-service-app/src/main/java/com/example/order_service_app/config/PaymentEventListener.java
@@ -3,6 +3,7 @@ package com.example.order_service_app.config;
 import com.example.order_service_app.service.OrderService;
 import com.example.order_service_client.dto.PaymentEventDto;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -10,6 +11,7 @@ import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
+
 
 @Slf4j
 @Component
@@ -20,7 +22,7 @@ public class PaymentEventListener {
 
     @KafkaListener(
             topics = "${app.kafka.topics.paymentTopic}",
-            groupId = "${spring.kafka.consumer.group-id}",
+            groupId = "order-service-group",
             containerFactory = "kafkaListenerContainerFactory"
     )
     public void handlePaymentEvent(@Payload String message , Acknowledgment acknowledgment){
@@ -46,3 +48,47 @@ public class PaymentEventListener {
     }
 
 }
+
+//@Slf4j
+//@Component
+//@RequiredArgsConstructor
+//public class PaymentEventListener {
+//
+//    private final OrderService orderService;
+//    private final TokenValidator tokenValidator; // Keycloak doğrulama servisi
+//
+//    @KafkaListener(
+//            topics = "${app.kafka.topics.paymentTopic}",
+//            groupId = "${spring.kafka.consumer.group-id}",
+//            containerFactory = "kafkaListenerContainerFactory"
+//    )
+//    public void handlePaymentEvent(@Payload String message , Acknowledgment acknowledgment){
+//        log.info("Received payment event message: {}", message);
+//
+//        try {
+//            ObjectMapper mapper = new ObjectMapper();
+//            JsonNode node = mapper.readTree(message);
+//
+//            String token = node.get("token").asText();
+//            String eventJson = node.get("message").toString();
+//
+//            if (!tokenValidator.validate(token)) {
+//                log.warn("Invalid token, discarding message");
+//                return;
+//            }
+//
+//            PaymentEventDto event = mapper.readValue(eventJson, PaymentEventDto.class);
+//
+//            if ("payment.succeeded".equals(event.getEventType())) {
+//                orderService.createOrderFromPayment(event);
+//                log.info("Order Created for Payment ID: {}", event.getPaymentId());
+//            }
+//
+//            acknowledgment.acknowledge();
+//
+//        } catch (Exception e) {
+//            log.error("Failed to process payment event message: {}", message, e);
+//        }
+//    }
+//}
+
--- backend/payment-service/payment-service-app/src/main/java/com/example/payment_service_app/config/KafkaProducerService.java
+++ backend/payment-service/payment-service-app/src/main/java/com/example/payment_service_app/config/KafkaProducerService.java
@@ -1,8 +1,13 @@
 package com.example.payment_service_app.config;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -13,3 +18,23 @@ public class KafkaProducerService {
         kafkaTemplate.send(topic,message);
     }
 }
+
+
+
+//@Service
+//@RequiredArgsConstructor
+//public class KafkaProducerService {
+//    private final KafkaTemplate<String , String> kafkaTemplate;
+//    private final KeycloakTokenService keycloakTokenService;
+//
+//    public void sendMessage(String topic , String message) throws JsonProcessingException {
+//        String token = keycloakTokenService.getClientCredentialsToken();
+//
+//        Map<String, Object> payload = new HashMap<>();
+//        payload.put("message", message);
+//        payload.put("token", token);
+//
+//        String jsonPayload = new ObjectMapper().writeValueAsString(payload);
+//        kafkaTemplate.send(topic, jsonPayload);
+//    }
+//}
